d8852225d7a75d5e8dfbcddc7e627b68f34deba2
Radosław Partyka <r.partyka30@gmail.com>
2020-01-03 15:16:18 +0100

    Keiko, command bugfix | Http request in another file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express"), app = express();
 const yandex = require("yandex-translate")(process.env.YandexApiKey), fs = require("fs"), https = require("https"), Sequelize = require("sequelize");
 const data = require("./data"), errors = require("./errors.js"), utils = require("./utils.js"),
-  StringReader = require("./stringReader.js");
+  StringReader = require("./stringReader.js"), requests = require("./requests.js");
 
 //Page requests
 app.use(express.static("webpage"));
@@ -63,107 +63,9 @@ const Heroes = db.define("HeroesData_2_1", {
 GData.sync();
 Heroes.sync();
 levels.sync();
-/* 
-app.get("/levels", (_request, response, {levels, Yui, Heroes}) => {
-  levels.findAll().then(res => {
-    res.sort((lelt, relt) => {
-      if (relt.lvl - lelt.lvl != 0) return relt.lvl - lelt.lvl;
-      else return relt.xp - lelt.xp;
-    });
-    response.send(res);
-  });
-});
- */
-require("./requests.js").forEach(elt => {
-  app.get(elt.adress, (request, response) => elt.execute(request, response, { levels, Yui, Heroes }))
-})
-app.get("/api/picture", (_request, response) => response.send(Yui.user.avatarURL));
-app.get("/api/guilds", (_request, response) => {
-  let text = Yui.guilds.reduce((acc, value) => {
-    let acc1 = acc;
-    elt = {
-      name: value.name, id: value.id,
-      NumOfChannels: value.channels.size, NumOdMembers: value.members.size,
-      NumOfEmojis: value.emojis.size, NumOfRoles: value.roles.size,
-      owner: {
-        id: value.ownerID,
-        tag: value.owner.user.tag
-      },
-      icon: value.iconURL,
-      isKeikoThere: value.members.find(elt => elt.id == "622783718783844356") ? true : false,
-      region: value.region
-    }
-    acc1.push(elt);
-    return acc1;
-  }, []);
-  response.send(text);
-});
-app.get("/hero", (_request, response) => { response.sendFile(__dirname + '/webpage/templateHero.html') })
-app.get("/edithero", (request, response) => {
-  Heroes.findOne({ where: { id: request.query.id } }).then(elt => {
-    if (!elt) response.sendFile(__dirname + '/webpage/findless.html')
-    else {
-      if (elt.userId == request.query.uid) response.sendFile(__dirname + '/webpage/templateEditHero.html')
-      else response.sendFile(__dirname + '/webpage/noperms.html')
-    }
-  })
 
-})
-app.get("/api/hero", (request, response) => {
-  Heroes.findOne({ where: { id: request.query.id } }).then(hero => {
-    if (hero) {
-      let editedHero = hero.dataValues;
-      delete editedHero.createdAt;
-      delete editedHero.updatedAt;
-      response.send(editedHero)
-    } else {
-      response.send(null)
-    }
-
-  });
-})
-app.get("/api/guildsettings", (request, response) => {
-  GData.findOne({ where: { guildId: request.query.id } }).then(settings => {
-    let editedSettings = settings.dataValues;
-    delete editedSettings.id;
-    delete editedSettings.createdAt;
-    delete editedSettings.updatedAt;
-    response.send(editedSettings)
-  });
-})
-
-app.get("/api/heroupdate", (request, response) => {
-  Heroes.findOne({ where: { id: request.query.id } }).then(elt => {
-    let hero = elt.dataValues;
-    GData.findOne({ where: { guildId: hero.guildId } }).then(guild => {
-      JSON.parse(decodeURIComponent(request.get('data'))).forEach(update => {
-        switch (update.type) {
-          case 'img':
-            hero.imageLink = update.dane;
-            break;
-          case 'field':
-            let parsedGFields = JSON.parse(guild.dataValues.fields);
-            let parsedFields = JSON.parse(hero.fields);
-            let temp = parsedFields.find(field => field.id == update.id);
-            if (temp) {
-              temp.data = update.dane;
-              parsedFields[parsedFields.findIndex(field => field.id == update.id)] = temp
-              hero.fields = JSON.stringify(parsedFields);
-            } else {
-              temp = { name: parsedGFields.find(felt => felt.id == update.id).name, data: update.dane, id: update.id }
-              parsedFields.push(temp)
-              hero.fields = JSON.stringify(parsedFields);
-              break;
-            }
-        }
-
-
-      })
-      Heroes.update(hero, { where: { id: request.query.id } });
-      response.send('Jest oke');
-      Heroes.sync();
-    })
-  })
+requests.forEach(elt => {
+  if (elt.beforeLogin) app.get(elt.adress, (request, response) => elt.execute(request, response, { levels, Yui, Heroes, GData }))
 })
 
 //Discord Bot
@@ -180,6 +82,9 @@ for (const file of commandFiles)
 Yui.on("ready", () => {
   let normal = { game: { name: "ludziuf", type: "watching" }, status: "online" };
   let service = { game: { name: "Przerwa techniczna :D", type: "playing" }, status: "idle" };
+  requests.forEach(elt => {
+    if (!elt.beforeLogin) app.get(elt.adress, (request, response) => elt.execute(request, response, { levels, Yui, Heroes, GData }))
+  })
   Yui.user.setPresence(normal);
   console.log("Logged & synced");
 });
